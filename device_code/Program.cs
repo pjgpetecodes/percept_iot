211598737ec884be525c2aa10858e0a1dcfa987f
Pete Gallagher <pete@pjgcreations.co.uk>
2021-09-19 12:17:57 +0100

    Removed supurfluous code from Device Control

--- device_code/Program.cs
+++ device_code/Program.cs
@@ -10,11 +10,9 @@ namespace device_messaging
 {
     class Program
     {
-        static string DeviceConnectionString = "ENTER YOU DEVICE CONNECTION STRING";
+        static string DeviceConnectionString = "ENTER YOUR DEVICE CONNECTION STRING";
         static int lightPin = 32;
-
-        GpioController controller;
-                                               
+          
         static async Task Main(string[] args)
         {
 
@@ -24,24 +22,6 @@ namespace device_messaging
             controller.Write(lightPin, PinValue.High);
             
             DeviceClient Client = DeviceClient.CreateFromConnectionString(DeviceConnectionString, Microsoft.Azure.Devices.Client.TransportType.Mqtt);
-
-            await Client.SetReceiveMessageHandlerAsync(async (Message message, object userContext) => {
-
-                var reader = new StreamReader(message.BodyStream);
-                var messageContents = reader.ReadToEnd();
-
-                Console.WriteLine($"Message Contents: {messageContents}");
-
-                Console.WriteLine("Message Propeties:");
-
-                foreach (var property in message.Properties)
-                {
-                    Console.WriteLine($"Key: {property.Key}, Value: {property.Value}");
-                }
-
-                await Client.CompleteAsync(message);
-
-            }, null);
 
             await Client.SetMethodHandlerAsync("ControlLight", (MethodRequest methodRequest, object userContext) => {
 
@@ -66,15 +46,11 @@ namespace device_messaging
 
             }, null);
 
+            Console.WriteLine("Waiting for Command");
+
             while (true)
             {
-                Console.WriteLine("Enter Message to Send (Empty Message to exit)");
-                var messageToSend = Console.ReadLine();
-
-                Message message = new Message(Encoding.ASCII.GetBytes(messageToSend));
-                message.Properties.Add("buttonEvent", "true");
-                Console.WriteLine("Sending Message {0}", messageToSend);
-                await Client.SendEventAsync(message);
+                
             }
 
         }
